5c5edcf0013670ec3f84462683f0488e982b95c6
DaliborHristovski <dalibor.hristovski@gmail.com>
2021-08-30 22:14:03 +0200

    Completed the register implementation under the login call

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # MSU
+
+
+To start Gulp run CMD or Bash on this location with:
+gulp watch.
+
+Any save to a change to HTML, CSS or JS will cause an inject into browser-sync.
+
+repo:
+ https://github.com/DaliborHristovski/MSU.git
+
+NPM version 6.7.0
+
+Clone repo:
+https://docs.github.com/en/get-started/getting-started-with-git/about-remote-repositories
+
+
--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
-
 const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
+const ejs = require("ejs");
 
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
+
+//const cookieParser = require("cookie-parser");
 const session = require("express-session");
 
 
@@ -14,9 +14,15 @@ const saltRounds = 10;
 const app = express();
 
 const path = require('path');
+const { response } = require("express");
 const router = express.Router();
 
+let subInfo;
+let appInfo;
+let studentInfo;
 
+app.set("view engine", "ejs");
+app.set("views", path.join('../docs/views'));
 
 app.use(express.json());
 app.use( cors({
@@ -25,14 +31,16 @@ app.use( cors({
     credentials: true,
   })
 );
-app.use(cookieParser());
-//app.use(bodyParser.urlencoded({ extended: true })); //maybe this is a problem
+//app.use(cookieParser());
+
+
 app.use(express.urlencoded({ extended: false }));
+
 
 app.use(
   session({
     key: "userId",
-    secret: "subscribe",
+    secret: "Water is not wet!",
     resave: false,
     saveUninitialized: false,
     cookie: {
@@ -48,13 +56,136 @@ const db = mysql.createConnection({
   database: "university"
 });
 
-app.use(express.static(path.resolve('../app')));
+async function getStudentInfo(x) {
+  db.query(
+    `    SELECT 
+    student.BrojNaIndex AS BrojNaIndex,
+    CONCAT(student.Ime,
+      ' ',
+      student.Prezime) AS 'ImeIPrezime'
+FROM
+    student
+WHERE
+    student.BrojNaIndex = ?;`,
+      x,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      studentInfo = result;
+      console.log(result);
+    }
+  );         
+};
+
+async function getAplicationInfo(x) {
+  db.query(
+    `    SELECT 
+    prijava_za_ispit.ID AS ID,
+    predmet.ImeNaPredmet AS ImeNaPredmet,
+    predmet.KodNaPredmet AS KodNaPredmet,
+    predmet.Semestar AS Semestar,
+    predmet.Krediti AS Krediti,
+    aktiviran_predmet.UcebnaGodina AS UcebnaGodina,
+    CONCAT(profesor.Ime, ' ', profesor.Prezime) AS Profesor
+FROM
+    (prijava_za_ispit
+    JOIN snap ON (prijava_za_ispit.IdStudentiNaPredmet = snap.IdStudentiNaPredmet)
+    JOIN aktiviran_predmet ON (snap.AP_ID = aktiviran_predmet.AP_ID)
+    JOIN student ON (snap.BrojNaIndex = student.BrojNaIndex)
+    JOIN predmet ON (aktiviran_predmet.KodNaPredmet = predmet.KodNaPredmet)
+    JOIN profesor ON (aktiviran_predmet.IDNaVaraboten = profesor.IDNaVraboten))
+WHERE
+    student.BrojNaIndex = ?;`,
+      x,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      appInfo = result;
+    }
+  );         
+};
+
+async function getSubjectInfo(x) {
+  db.query(
+    `SELECT 
+    predmet.ImeNaPredmet,
+    aktiviran_predmet.AP_ID
+FROM (university.snap
+        JOIN aktiviran_predmet ON (snap.AP_ID = aktiviran_predmet.AP_ID)
+        JOIN predmet ON (aktiviran_predmet.KodNaPredmet = predmet.KodNaPredmet)
+        JOIN student ON (snap.BrojNaIndex = student.BrojNaIndex)
+        )where aktiviran_predmet.AP_ID NOT IN(    
+    SELECT 
+		aktiviran_predmet.AP_ID
+    FROM
+        (
+prijava_za_ispit
+        JOIN snap ON (prijava_za_ispit.IdStudentiNaPredmet = snap.IdStudentiNaPredmet)
+        JOIN aktiviran_predmet ON (snap.AP_ID = aktiviran_predmet.AP_ID)
+        JOIN student ON (snap.BrojNaIndex = student.BrojNaIndex)
+        JOIN predmet ON (aktiviran_predmet.KodNaPredmet = predmet.KodNaPredmet)
+        JOIN profesor ON (aktiviran_predmet.IDNaVaraboten = profesor.IDNaVraboten)
+        )
+        )AND student.BrojNaIndex = ?;`,
+      x,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      subInfo = result;
+    }
+  );     
+      
+};
+
+
+app.use(express.static(path.resolve('../docs')));
 //router.get('/',function(req,res){res.sendFile(path.resolve('../app/index.html'));});
+app.get("/", (req, res) =>{
+  //res.sendFile(path.join(__dirname,'../docs/index.html'));
+  }
+);
 
+//no longer used and it is integrated within the login call
+// app.post("/register", (req, res) => {
+//   const username = req.body.username;
+//   const password = req.body.password;
 
-app.post("/register", (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
+//   bcrypt.hash(password, saltRounds, (err, hash) => {
+//     if (err) {
+//       console.log(err);
+//     }
+
+//     db.query(
+//       "INSERT INTO login (username, password) VALUES (?,?)",
+//       [username, hash],
+//       (err, result) => {
+//         console.log(err);
+//       }
+//     );
+//   });
+// });
+
+app.get("/login", (req, res) => {
+  if (req.session.user) {
+    res.send({ loggedIn: true, user: req.session.user });
+  } else {
+    res.send({ loggedIn: false });
+  }
+});
+
+//Need to add a button that calls this in the pages
+app.get("/logout", function(req,res) {
+  req.session.destroy();
+  res.status(200).send('ok');  
+});
+
+//this is what will get called to input into the db.login for user that joins for the first time
+function Regis(userName, inputPassword) {
+  const username = userName;
+  const password = inputPassword;
 
   bcrypt.hash(password, saltRounds, (err, hash) => {
     if (err) {
@@ -69,22 +200,15 @@ app.post("/register", (req, res) => {
       }
     );
   });
-});
-
-app.get("/login", (req, res) => {
-  if (req.session.user) {
-    res.send({ loggedIn: true, user: req.session.user });
-  } else {
-    res.send({ loggedIn: false });
-  }
-});
-app.get(("/login", (req, res) => {
-  res.send("why are we here just to suffer");
-}));
+};
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
+  
+  getSubjectInfo(username);
+  getAplicationInfo(username);
+  getStudentInfo(username);
 
   db.query(
     "SELECT * FROM login WHERE username = ?;",
@@ -95,20 +219,48 @@ app.post("/login", (req, res) => {
       }
       
       if (result.length > 0) {
+        //comparing encripted values
         bcrypt.compare(password, result[0].password, (error, response) => {
           if (response) {
             req.session.user = result;
-            console.log(req.session.user);
-            res.send(result);
+            studentIndex = req.session.user[0].username; 
+            
+            //res.send(result);
+            //res.sendFile(path.resolve('../docs/index.html'))
+            //res.render(path.resolve('../docs/views/home.ejs'))
+            res.render("home",{
+              prInfo: subInfo,
+              appInfo: appInfo,
+              sInfo: studentInfo
+            });
           } else {
             res.send({ message: "Wrong username/password combination!" });
           }
         });
-      } else {
-        res.send({ message: "User doesn't exist" });
-      }
-    }
-  );
+      }      
+       else 
+      {
+        db.query(
+          "SELECT * From student WHERE student.BrojNaIndex = ?;", 
+          username,
+          (err, result) => {
+            if (err) { res.send({ err: err });
+            }
+            if (result.length > 0 && result.EMBG == password) {
+              Regis(username,password);
+              res.render("home",{
+                prInfo: subInfo,
+                appInfo: appInfo,
+                sInfo: studentInfo
+              });
+            }else{
+              res.send({ message: "User doesn't exist" });
+            };
+        
+           }
+        )
+      }}
+  )
 });
 
 
