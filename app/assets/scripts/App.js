5bfdccab2ad75613d5bde91f14001c8755c68c0d
DaliborHristovski <dalibor.hristovski@gmail.com>
2021-10-01 05:03:09 +0200

    Added some visual elements on the index page and few changes to the other 2 templates, added explanations and corrected few mistakes in the cssleft over from incomplete entries

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -10,6 +10,7 @@ import Modal from "./modules/Modal";
 let mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"),"85%");
 new RevealOnScroll($(".testimonial"), "60%");
+
 let stickyHeader = new StickyHeader();
 let modal = new Modal();
 
